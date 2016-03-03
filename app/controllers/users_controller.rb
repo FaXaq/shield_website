3a1beac33f2777fd4f4448fb117fb95ab51a0919
Marin Procureur <marin.procureur@enioka.com>
2016-03-03 14:56:55 +0100

    User register ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @user = User.new
+    render 'new'
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You better rock out !"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmationx)
+  end
+end
