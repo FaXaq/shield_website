2aeb9f0814e5a1a723fe37fedba96170df2c02ff
Marin Procureur <marin.procureur@enioka.com>
2016-04-15 14:10:50 +0200

    Add default cover for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
             presence: true
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"
-                                     }, default_url: "https://source.unsplash.com/random"
+                                     }, default_url: "banner.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates_attachment :avatar,
